3c2ea6c389699f59b5ebafcff9ec2cab85c22a8e
leewooyup <zjadlspun1114@naver.com>
2023-08-03 17:24:09 +0900

    20230803- Service와 Dao 인터페이스로 규격맞추고, Cust Dto 추가 및 lombok 의존성 추가

---
+++ prac01/src/app/cust/CustDaoImpl.java
@@ -0,0 +1,40 @@
+package app.cust;
+
+import java.util.List;
+
+import app.dto.Cust;
+import app.frame.DaoFrame;
+
+public class CustDaoImpl implements DaoFrame<String, Cust> {
+
+	@Override
+	public int insert(Cust v) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Cust v) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(String k) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Cust select(String k) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cust> select() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ prac01/src/app/cust/CustServiceImpl.java
@@ -0,0 +1,40 @@
+package app.cust;
+
+import java.util.List;
+
+import app.dto.Cust;
+import app.frame.ServiceFrame;
+
+public class CustServiceImpl implements ServiceFrame<String, Cust>{
+
+	@Override
+	public void register(Cust v) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modify(Cust v) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(String k) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Cust get(String k) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cust> get() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
